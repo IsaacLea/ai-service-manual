6c795630fa8cbd85d3378d6b079f341ef52ca7a6
Isaac <isaaclea123@gmail.com>
2025-05-05 14:37:01 -0700

    updated font color of messages

--- src/app/components/MessageDisplay.tsx
+++ src/app/components/MessageDisplay.tsx
@@ -5,7 +5,7 @@ import { QueryResult } from "../types";
 const MessageDisplay: React.FC<QueryResult> = ({ message }) => {
   return (
     <div className="mt-4 p-4 border rounded bg-gray-100">
-      <p>{message}</p>
+      <p className="text-black">{message}</p>
     </div>
   );
 };
