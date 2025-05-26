dbd9ee95fb77dd59c4ecf96312da99cf8b455158
Isaac Lea <isaac.lea@nttdata.com>
2025-05-26 10:49:51 -0700

    Updated home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@ import ChatSelection from "./components/ChatSelection";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <header className="mb-8 flex flex-col items-center space-y-4">
 
-        <h1 className="text-4xl font-extrabold text-blue-800 drop-shadow-lg">
+      <main className="w-full max-w-xl bg-white rounded-xl shadow-lg p-8 items-center flex flex-col space-y-4">
+
+        <h1 className="text-2xl text-black drop-shadow-lg">
           AI Powered Content
         </h1>
 
-      </header>
-      <main className="w-full max-w-xl bg-white rounded-xl shadow-lg p-8">
         <ChatSelection />
       </main>
     </div>
