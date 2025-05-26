07d9099697d121b55e82b9134a363d9a841c4326
Isaac <isaaclea123@gmail.com>
2025-05-26 13:50:14 -0700

    Prevent scroll on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-50 to-blue-200 overflow-y-hidden">
+        <div className="flex flex-col h-[100dvh] bg-gradient-to-br from-blue-50 to-blue-200">
           <header className="w-full flex justify-between items-center px-8 py-2 shadow-md bg-gradient-to-br from-blue-50 to-blue-200">
             <Link
               href="/"
