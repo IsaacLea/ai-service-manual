d560b5d8d6f31d97b1669e34776cb0cb772a5688
Isaac <isaaclea123@gmail.com>
2025-05-26 22:31:11 -0700

    Fixed width of login form

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -15,75 +15,74 @@ export default function LoginForm() {
     );
 
     return (
-        <div className="grid justify-items-center h-full">
 
-            <form action={formAction} className="bg-white p-6 rounded shadow-md">
-                <h1>Login</h1>
+        <form action={formAction} className="flex-1 h-full w-full max-w-xl bg-white rounded-xl shadow-lg p-8 items-center flex flex-col space-y-4 text-black">
+            <h1>Login</h1>
 
-                <div className="w-full">
-                    <div>
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="userName"
-                        >
-                            User name
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="userName"
-                                type="userName"
-                                name="userName"
-                                placeholder="Enter your user name"
-                                required
-                            />
+            <div className="w-full">
+                <div>
+                    <label
+                        className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                        htmlFor="userName"
+                    >
+                        User name
+                    </label>
+                    <div className="relative">
+                        <input
+                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                            id="userName"
+                            type="userName"
+                            name="userName"
+                            placeholder="Enter your user name"
+                            required
+                        />
 
-                        </div>
-                    </div>
-                    <div className="mt-4">
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="password"
-                        >
-                            Password
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="password"
-                                type="password"
-                                name="password"
-                                placeholder="Enter password"
-                                required
-                                minLength={6}
-                            />
-
-                        </div>
                     </div>
                 </div>
+                <div className="mt-4">
+                    <label
+                        className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                        htmlFor="password"
+                    >
+                        Password
+                    </label>
+                    <div className="relative">
+                        <input
+                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                            id="password"
+                            type="password"
+                            name="password"
+                            placeholder="Enter password"
+                            required
+                            minLength={6}
+                        />
 
-                <input type="hidden" name="redirectTo" value={callbackUrl} />
-                <div className="my-4" />
-                <button
-                    type="submit"
-                    className="px-4 py-2 rounded text-white bg-blue-500 hover:bg-blue-600"
-                    aria-disabled={isPending}>
-                    Log in
-                </button>
-
-                <div
-                    className="flex h-8 items-end space-x-1"
-                    aria-live="polite"
-                    aria-atomic="true"
-                >
-                    {errorMessage && (
-                        <>
-                            <p className="text-sm text-red-500">{errorMessage}</p>
-                        </>
-                    )}
+                    </div>
                 </div>
-            </form>
+            </div>
 
-        </div>
+            <input type="hidden" name="redirectTo" value={callbackUrl} />
+            <div className="my-4" />
+            <button
+                type="submit"
+                className="px-4 py-2 rounded text-white bg-blue-500 hover:bg-blue-600"
+                aria-disabled={isPending}>
+                Log in
+            </button>
+
+            <div
+                className="flex h-8 items-end space-x-1"
+                aria-live="polite"
+                aria-atomic="true"
+            >
+                {errorMessage && (
+                    <>
+                        <p className="text-sm text-red-500">{errorMessage}</p>
+                    </>
+                )}
+            </div>
+        </form>
+
+
     )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from '../components/LoginForm';
 
 export default function LoginPage() {
     return (
-        <div className="grid justify-items-center h-full">
+        <div className="grid justify-items-center">
             {/* {This component is wrapped with React <Suspense> because it will access information from the incoming request (URL search params)} */}
             <Suspense>
                 <LoginForm />
