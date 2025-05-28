464b26ef83646db57cacc623bd789d79a6c23628
Isaac Lea <isaac.lea@nttdata.com>
2025-05-28 13:11:44 -0700

    removed console.log

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -14,7 +14,6 @@ export const authConfig = {
                 if (isLoggedIn) return true;
                 return false; // Redirect unauthenticated users to login page
             } else {
-                console.log(nextUrl);
                 return true
                 //return Response.redirect(new URL('/admin', nextUrl));
             }
